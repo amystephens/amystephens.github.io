bb5e8f9543aad94aa19e3a861f27784b2e54c4a6
amystephens <akagundy@byui.edu>
2020-10-02 17:51:00 -0600

    First Upload Lesson 3 Weather Website

---
+++ lesson 3/js/dates.js
@@ -0,0 +1,5 @@
+//shows last updated for footer
+document.getElementById("lastModified").textContent = document.lastModified;
+
+//shows current year for copyright
+document.getElementById("currentYear").textContent = new Date().getFullYear();
