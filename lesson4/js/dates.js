58af025037c8715f8bae61eaede4be693a946c41
amystephens <akagundy@byui.edu>
2020-10-10 17:52:11 -0600

    javascript date and color

--- lesson4/js/dates.js
+++ lesson4/js/dates.js
@@ -1,14 +1,15 @@
 //shows last updated for footer
 // document.getElementById("lastModified").textContent = document.lastModified;
 
-//shows last updated for footer with day, number, month year - new format for Lesson 4
-var options = {
+//shows last updated for footer with day, number, month year - new format for Lesson 4 - use en-GB to get the date before the month
+const options = {
     weekday: 'long', 
     day: 'numeric', 
     month: 'long', 
     year: 'numeric',
-}
-document.getElementById('lastModified').textContent = new Date().toLocaleDateString('en-US', options);
+};
+
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
 
 //shows current year for copyright
 document.getElementById("currentYear").textContent = new Date().getFullYear();
---
+++ lesson4/js/jacob.js
@@ -0,0 +1,14 @@
+function toggleMenu() {
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+var options = {
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long', 
+    year: 'numeric',
+ 
+}
+
+document.getElementById('currentDate').innerHTML = new Date().toLocaleDateString('en-US', options);
