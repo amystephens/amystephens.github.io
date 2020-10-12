de67fad36b679939bb87a41409b26f06cf271739
amystephens <akagundy@byui.edu>
2020-10-12 16:12:29 -0600

    Sat late night lesson 3 changes and Mon favicon

---
+++ lesson5/js/dates.js
@@ -0,0 +1,16 @@
+//shows last updated for footer
+// document.getElementById("lastModified").textContent = document.lastModified;
+
+//shows last updated for footer with day, number, month year - new format for Lesson 4 - use en-GB to get the date before the month
+const options = {
+    weekday: 'long', 
+    day: 'numeric', 
+    month: 'long', 
+    year: 'numeric',
+};
+
+document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-GB', options);
+
+//shows current year for copyright
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
