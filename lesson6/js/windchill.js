97664f30b1db58fb24cea3df813ac73a6fa260fd
amystephens <akagundy@byui.edu>
2020-10-23 16:31:43 -0600

    windchill javascript works wahoo

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,37 +1,9 @@
-function windChill() {
-    let t = parseInt(document.getElementById("hightemp").textContent); //air average/high temp in F
-    let s = parseInt(document.getElementById("windspeed").textContent); // wind speed in mph
-}
-
+let t = parseFloat(document.getElementById('currenttemp').textContent); // current temp in F
+let s = parseFloat(document.getElementById("windspeed").textContent); // wind speed in mph
+let wc = "N/A";
+    
 if (t <= 50 && s >= 3.0) {
-   return windchill = Math.round(35.74 + (.6215 * t) - (35.75 * (Math.pow(s,.16))) + (.4275 * t * (Math.pow(s, .16))));
+    let calc = (35.74 + (.6215 * t)) - (35.75 * (Math.pow(s, .16))) + (.4275 * (t * (Math.pow(s, .16))));
+    wc = Math.round(calc);
 }
-else {
-    windchill = "NA";
-}
-document.getElementById("windchill").textContent = windchill;
-
-// Old One
-<script type="text/javascript">
-//input: get a temp and wind speed from the text fields in html
-function compute() {
-    temp1 = parseInt(document.getElementById("tempIn").value);
-    speed1 = parseInt(document.getElementById("windIn").value);
-    //processing: call the second function and store value in this function's variable
-    windyChill = windChill(temp1, speed1);
-    //output: output the value to the div
-    document.getElementById("output").innerHTML = "It feels like " + windyChill + "° F.";
-}
-
-
-function windChill(tempF, speed) {
-//input: take the values from function compute()
-t = tempF;
-s = speed;
-//processing: compute the wind chill factor
-windChillFactor = Math.round(35.74 + (.6215 * t) - (35.75 * (Math.pow(s,.16))) + (.4275 * t * (Math.pow(s, .16))));
-//output: return the value to the windyChill variable in the function compute()
-return windChillFactor;
-}
-
-</script>
+document.getElementById("windchill").innerHTML = wc;
