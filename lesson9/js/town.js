bfe3b7dac49ee062e9341f2de5e0960141721a56
amystephens <akagundy@byui.edu>
2020-11-14 12:43:50 -0700

    css adjustments index 9 page

--- lesson9/js/town.js
+++ lesson9/js/town.js
@@ -18,6 +18,8 @@ fetch(requestURL)
         let population = document.createElement('p');
         let rainfall = document.createElement('p');
         let image = document.createElement("img");
+        let cardText = document.createElement("div");
+        let cardImg = document.createElement("div");
         
         h2.textContent = filtered[i].name;
         motto.textContent =  filtered[i].motto;
@@ -27,56 +29,20 @@ fetch(requestURL)
         image.setAttribute('src', 'images/' + filtered[i].photo);
         image.setAttribute('alt', 'Picture of ' +  filtered[i].name);
 
-        card.appendChild(h2);
-        card.appendChild(motto);
-        card.appendChild(founded);
-        card.appendChild(population);
-        card.appendChild(rainfall);
-        card.appendChild(image);
+        cardText.appendChild(h2);
+        cardText.appendChild(motto);
+        cardText.appendChild(founded);
+        cardText.appendChild(population);
+        cardText.appendChild(rainfall);
+        card.appendChild(cardText);
+        cardImg.appendChild(image);
+        card.appendChild(cardImg);
+        
+        cardText.setAttribute("class", "cardText");
+        motto.setAttribute("class", "motto");
+        cardImg.setAttribute("class", "cardImg");
         
         document.querySelector('div.cards').appendChild(card);
     }
 
   });
-
-  fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const town = jsonObject['towns'];
-    const filtered = town.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
-
-    //town.forEach (town =>) {
-    for (let i = 0; i < filtered.length; i++ ) {
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let motto = document.createElement('p');
-        let founded = document.createElement('p');
-        let population = document.createElement('p');
-        let rainfall = document.createElement('p');
-        let image = document.createElement("img");
-        
-        h2.textContent = filtered[i].name;
-        motto.textContent =  filtered[i].motto;
-        founded.textContent = 'Year Founded: ' + filtered[i].yearFounded;
-        population.textContent = 'Population: ' + filtered[i].currentPopulation;
-        rainfall.textContent = 'Annual Rain Fall: ' + filtered[i].averageRainfall;
-        image.setAttribute('src', 'images/' + filtered[i].photo);
-        image.setAttribute('alt', 'Picture of ' +  filtered[i].name);
-
-        card.appendChild(h2);
-        card.appendChild(motto);
-        card.appendChild(founded);
-        card.appendChild(population);
-        card.appendChild(rainfall);
-        card.appendChild(image);
-        
-        document.querySelector('div.cardsmedium').appendChild(card);
-    }
-
-  });
-  
-
-  
