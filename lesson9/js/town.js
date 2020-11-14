c2485bbb839297f4278d583a870e144c685c9633
amystephens <akagundy@byui.edu>
2020-11-13 23:07:41 -0700

    assignment draft

--- lesson9/js/town.js
+++ lesson9/js/town.js
@@ -20,7 +20,7 @@ fetch(requestURL)
         let image = document.createElement("img");
         
         h2.textContent = filtered[i].name;
-        motto.textContent =  filtered[i].name;
+        motto.textContent =  filtered[i].motto;
         founded.textContent = 'Year Founded: ' + filtered[i].yearFounded;
         population.textContent = 'Population: ' + filtered[i].currentPopulation;
         rainfall.textContent = 'Annual Rain Fall: ' + filtered[i].averageRainfall;
@@ -39,6 +39,44 @@ fetch(requestURL)
 
   });
 
+  fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const town = jsonObject['towns'];
+    const filtered = town.filter(town => (town.name == 'Preston' || town.name == 'Soda Springs' || town.name == 'Fish Haven'));
+
+    //town.forEach (town =>) {
+    for (let i = 0; i < filtered.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let motto = document.createElement('p');
+        let founded = document.createElement('p');
+        let population = document.createElement('p');
+        let rainfall = document.createElement('p');
+        let image = document.createElement("img");
+        
+        h2.textContent = filtered[i].name;
+        motto.textContent =  filtered[i].motto;
+        founded.textContent = 'Year Founded: ' + filtered[i].yearFounded;
+        population.textContent = 'Population: ' + filtered[i].currentPopulation;
+        rainfall.textContent = 'Annual Rain Fall: ' + filtered[i].averageRainfall;
+        image.setAttribute('src', 'images/' + filtered[i].photo);
+        image.setAttribute('alt', 'Picture of ' +  filtered[i].name);
+
+        card.appendChild(h2);
+        card.appendChild(motto);
+        card.appendChild(founded);
+        card.appendChild(population);
+        card.appendChild(rainfall);
+        card.appendChild(image);
+        
+        document.querySelector('div.cardsmedium').appendChild(card);
+    }
+
+  });
   
 
   
