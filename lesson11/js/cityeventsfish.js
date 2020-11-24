e43951364f6a162f1b00a25f3b3d4b0a18ef82b5
amystephens <akagundy@byui.edu>
2020-11-24 14:16:37 -0700

    events JSON and week 11 updates

---
+++ lesson11/js/cityeventsfish.js
@@ -0,0 +1,34 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+function cityEvents(cityName) {
+
+  fetch(requestURL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonObject) {
+
+      const town = jsonObject['towns'];
+      const filtered = town.filter(town => (town.name == cityName));
+
+      let h5 = document.createElement('h5');
+      let eventUl = document.createElement('ul');
+
+      h5.textContent = 'Upcoming Events in ' + filtered[0].name;
+
+      for (let i = 0; i < filtered[0].events.length; i++) {
+        let eventLi = document.createElement('li');
+        let cityHtml = document.getElementsByClassName("cityname");
+        let cityName = filtered[0].name.includes(cityHtml);
+
+        eventLi.textContent = filtered[0].events[i];
+        eventUl.appendChild(eventLi);
+      }
+
+      document.querySelector('div.events').appendChild(h5);
+      document.querySelector('div.events').appendChild(eventUl);
+
+    });
+}
+
+cityEvents('Fish Haven');
---
+++ lesson11/js/weatherapisoda.js
@@ -0,0 +1,61 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5607916&appid=9a26765da4794e6d99960311611fab93&units=imperial';
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5607916&appid=9a26765da4794e6d99960311611fab93&units=imperial';
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject); //use this to check the console
+        //weather overlay box
+        document.getElementById('current-temp').innerHTML = Math.round(jsObject.main.temp) + " &#176;F";
+        document.getElementById('windspeed').textContent = Math.round(jsObject.wind.speed);
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        //Currently: description
+        document.getElementById('currdescription').textContent = jsObject.weather[0].description;
+        //calculate windchill
+        let t = (Math.round(jsObject.main.temp));
+        let s = (Math.round(jsObject.wind.speed));
+        let wc = "N/A";
+        if (t <= 50 && s >= 3.0) {
+            let calc = (35.74 + (.6215 * t)) - (35.75 * (Math.pow(s, .16))) + (.4275 * (t * (Math.pow(s, .16))));
+            wc = Math.round(calc) + " &#176;F";
+        }
+        document.getElementById("windchill").innerHTML = wc;
+    });
+
+fetch(forecastURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //forecast box
+        //forecast day of the week
+        
+        var weekday = new Array(7);
+        weekday[0] = "Sun";
+        weekday[1] = "Mon";
+        weekday[2] = "Tue";
+        weekday[3] = "Wed";
+        weekday[4] = "Thu";
+        weekday[5] = "Fri";
+        weekday[6] = "Sat";
+        
+
+        var gridTop = document.getElementsByClassName("gridtop");
+        var forecastImg = document.getElementsByClassName("forecastimg");
+        var data = jsObject.list.filter(item => item.dt_txt.includes("18:00:00"));
+        var forecastTemp = document.getElementsByClassName("forecasttemp");
+
+        for (var i = 0; i < data.length; i++) {
+            var d = new Date(data[i].dt_txt);
+           
+            gridTop[i].textContent = weekday[d.getDay()];
+
+            //forecast icon and then description
+            const imagesfc = 'https://openweathermap.org/img/w/' + data[i].weather[0].icon + '.png'; // note the concatenation
+            const description = data[i].weather[0].description; // note how we reference the weather array
+            forecastImg[i].setAttribute('src', imagesfc); // focus on the setAttribute() method
+            forecastImg[i].setAttribute('alt', description);
+
+            //forecast temp
+            forecastTemp[i].innerHTML = Math.round(data[i].main.temp) + " &#176;F";
+        }
+
+    });
